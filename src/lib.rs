004b62e50a0cca03108d94e1252981b9fd2a008a
khai96_ <hvksmr1996@gmail.com>
2017-06-25 21:38:19 +0700

    Modularize

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
+pub mod lib {
+    pub struct HelloWorld;
+
+    impl ToString for HelloWorld {
+        fn to_string(&self) -> String { "Hello, World!!".to_string() }
+    }
+
+    impl HelloWorld {
+        pub fn print(&self) {
+            println!("{}", self.to_string());
+        }
+
+        pub fn create_printer() -> Box<Fn()> {
+            Box::new(|| (HelloWorld {}).print())
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-struct HelloWorld;
-
-impl ToString for HelloWorld {
-    fn to_string(&self) -> String { "Hello, World!!".to_string() }
-}
-
-impl HelloWorld {
-    fn print(&self) {
-        println!("{}", self.to_string());
-    }
-
-    fn create_printer() -> Box<Fn()> {
-        Box::new(|| (HelloWorld {}).print())
-    }
-}
+extern crate hello;
+use hello::lib::HelloWorld;
 
 fn main() {
     HelloWorld::create_printer()();
