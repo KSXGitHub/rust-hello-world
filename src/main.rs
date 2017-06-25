72120566f6117b3b390c39228295aa5385c1d0f7
khai96_ <hvksmr1996@gmail.com>
2017-06-25 17:05:46 +0700

    Be more complicated

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct HelloWorld;
+
+impl HelloWorld {
+    fn to_string (&self) -> &str { "Hello, World!!" }
+
+    fn print(&self) {
+        println!("{}", self.to_string());
+    }
+}
+
 fn main() {
-    println!("Hello, World!!");
+    let hw = HelloWorld {};
+    hw.print();
 }
