818f1b612b769cb08cf798a788829b12ce92ca7c
khai96_ <hvksmr1996@gmail.com>
2017-06-25 21:55:54 +0700

    Factorial

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 extern crate hello_world;
-use hello_world::lib::HelloWorld;
+pub use hello_world::lib::HelloWorld;
+pub use self::factorial as fac;
+
+pub fn factorial(n: u8) -> u64 {
+    let nn = n as u64;
+    if nn > 1 {
+        nn * fac(n - 1)
+    } else {
+        1
+    }
+}
 
 fn main() {
+    for n in 0..10 {
+        println!("{}! = {}", n, fac(n));
+    }
     HelloWorld::create_printer()();
 }
