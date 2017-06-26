35926ef30233213e1e379e02666859a9614399b6
khai96_ <hvksmr1996@gmail.com>
2017-06-26 17:09:26 +0700

    Equally cast

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use hello_world::factorial::fac;
 use hello_world::fibonacci::fib;
 
 pub fn main () {
-    let (begin, end) = (0 as u8, 16 as u8);
+    let (begin, end) = (0, 16);
 
     let vecres = (begin..end).collect::<Vec<_>>();
     println!("Vector: {:?}", vecres);
@@ -11,9 +11,9 @@ pub fn main () {
     let sqrres = (begin..end).map(|x| x * x).collect::<Vec<_>>();
     println!("Square: {:?}", sqrres);
 
-    let facres = (begin..end).map(fac).collect::<Vec<u64>>();
+    let facres = ((begin as u8)..(end as u8)).map(fac).collect::<Vec<_>>();
     println!("Factorial: {:?}", facres);
 
-    let fibres = ((begin as u16)..(end as u16)).map(fib).collect::<Vec<u64>>();
+    let fibres = ((begin as u16)..(end as u16)).map(fib).collect::<Vec<_>>();
     println!("Fibonacci: {:?}", fibres);
 }
